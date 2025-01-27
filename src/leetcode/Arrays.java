e90c04196a298065c638d8bf54f0891eed9a608d
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-01-27 16:43:11 -0500

    Added new solutions

--- src/leetcode/Arrays.java
+++ src/leetcode/Arrays.java
@@ -2729,4 +2729,17 @@ public class Arrays {
 
         return media;
     }
+
+    public List<Integer> stableMountains(int[] height, int threshold) {
+
+        List<Integer> mountains = new ArrayList<>();
+
+        for (int i = 1; i < height.length; i++) {
+            if (height[i-1] >= threshold) {
+                mountains.add(i);
+            }
+        }
+
+        return mountains;
+    }
 }
