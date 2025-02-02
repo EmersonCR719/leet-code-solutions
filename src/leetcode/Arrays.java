658beff11aca1b092f13e9e2767812e19b2239a7
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-02-02 15:01:14 -0500

    Added new solutions

--- src/leetcode/Arrays.java
+++ src/leetcode/Arrays.java
@@ -1090,10 +1090,6 @@ public class Arrays {
         return true;
     }
 
-    public boolean check(int[] nums) {
-        return false;
-    }
-
     public int largestSumAfterKNegations(int[] nums, int k) {
         int sumMax = 0;
         java.util.Arrays.sort(nums);
@@ -2793,8 +2789,28 @@ public class Arrays {
 
     public boolean uniqueOccurrences(int[] arr) {
 
-        
+
 
         return false;
     }
+
+    public boolean check(int[] nums) {
+
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] > nums[(i+1) % n]){
+                count++;
+            }
+
+            if (count > 1) return false;
+        }
+
+
+        return true;
+    }
+
+    
 }
