7e2745a8ff8063792023b875221607dc80f3a35d
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-02-13 14:15:41 -0500

    Added new solutions

--- src/leetcode/Arrays.java
+++ src/leetcode/Arrays.java
@@ -2956,4 +2956,25 @@ public class Arrays {
         return maxSum;
     }
 
+    public int minOperations(int[] nums, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int numOfOperations = 0;
+
+        for (int num : nums) {
+            if (num < k) pq.add(num);
+        }
+
+        while (!pq.isEmpty()) {
+            int x = pq.poll();
+            numOfOperations++;
+            if (pq.isEmpty()) break;
+            int y = pq.poll();
+            long operation = (x * 2L) + y;
+            if (operation < k) pq.add((int) operation);
+        }
+        return numOfOperations;
+    }
+
 }
--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
         Arrays arr = new Arrays();
 
-        int[] nums1 = {1,7,5,9,2,12,3};
+        int[] nums1 = {1000000000,999999999,1000000000,999999999,1000000000,999999999};
 
-        int[] nums2 = {3,4};
+        int[] nums2 = {3, 4};
 
         String s1 = "abcd", s2 = "dcba";
 
-        System.out.println(arr.areAlmostEqual(s1, s2));
+        System.out.println(arr.minOperations(nums1, 20));
 
     }
 }
