b4ba25d2851de8818edc7ddbb91d2bc81206b210
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-02-20 16:47:30 -0500

    Added new solutions

--- src/leetcode/array/Arrays.java
+++ src/leetcode/array/Arrays.java
@@ -3054,4 +3054,21 @@ public class Arrays {
         return total;
     }
 
+    public String findDifferentBinaryString(String[] nums) {
+
+        StringBuilder sb = new StringBuilder();
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if(nums[i].charAt(i)=='0'){
+                sb.append('1');
+            }else{
+                sb.append('0');
+            }
+        }
+
+        return sb.toString();
+    }
+
 }
