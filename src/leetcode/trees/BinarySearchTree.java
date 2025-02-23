579052af292e0d0cf3836bc8922bee102c99d956
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-02-22 20:49:00 -0500

    Added new solutions

--- src/leetcode/trees/BinarySearchTree.java
+++ src/leetcode/trees/BinarySearchTree.java
@@ -42,7 +42,53 @@ public class BinarySearchTree {
         return inorder;
     }
 
-    public TreeNode recoverFromPreorder(String traversal) {
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+
         return null;
+    }
+
+    public TreeNode recoverFromPreorder(String traversal) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        int i = 0, n = traversal.length();
+
+        while (i < n) {
+            int depth = 0;
+
+            while (i < n && traversal.charAt(i) == '-') {
+                depth++;
+                i++;
+            }
+
+            int value = 0;
+            while (i < n && Character.isDigit(traversal.charAt(i))) {
+                value = value * 10 + traversal.charAt(i) - '0';
+                i++;
+            }
+
+            TreeNode node = new TreeNode(value);
+
+            while (stack.size() > depth) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                TreeNode parent = stack.peek();
+                if (parent.left == null) {
+                    parent.left = node;
+                } else {
+                    parent.right = node;
+                }
+            }
+
+            stack.push(node);
+        }
+
+        while (stack.size() > 1) {
+            stack.pop();
+        }
+
+        return stack.peek();
     }
 }
