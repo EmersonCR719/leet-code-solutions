9bf7a06c4301a451bb48363eece49ed2013d8a9e
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-03-10 16:50:22 -0500

    Added new solutions

--- src/leetcode/string/Strings.java
+++ src/leetcode/string/Strings.java
@@ -641,4 +641,19 @@ public class Strings {
         return maxVowels;
     }
 
+    public int firstUniqChar(String s) {
+
+        int[] counts = new int[26];
+
+        for (char c : s.toCharArray()) {
+            counts[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (counts[s.charAt(i) - 'a'] == 1) return i;
+        }
+
+        return -1;
+    }
+
 }
