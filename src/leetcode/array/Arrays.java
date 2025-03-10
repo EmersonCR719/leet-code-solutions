3bf1ca3cd60284d5e9937ebfa09d90c0e0c489c4
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-03-10 14:43:49 -0500

    Added new solutions

--- src/leetcode/array/Arrays.java
+++ src/leetcode/array/Arrays.java
@@ -3146,4 +3146,25 @@ public class Arrays {
         return mergedArray;
     }
 
+    public double findMaxAverage(int[] nums, int k) {
+
+        int n = nums.length;
+        double maxAverage;
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        maxAverage = (double) sum / k;
+
+        for (int i = k; i < n; i++) {
+            sum = sum - nums[i -k] + nums[i];
+            maxAverage = Math.max(maxAverage,(double) sum/k);
+        }
+
+
+        return maxAverage;
+    }
+
 }
