80dd3b5cf6267aa5e21df1dfffc2047b30029e69
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-03-17 23:54:45 -0500

    Added new solutions

--- src/leetcode/bitmanipulation/BitManipulation.java
+++ src/leetcode/bitmanipulation/BitManipulation.java
@@ -69,4 +69,21 @@ public class BitManipulation {
 
         return count;
     }
+
+    public boolean isPowerOfTwo(int n) {
+        /*
+        if (n <= 0) return false;
+
+        int count = 0;
+
+        while (n != 0) {
+            n &= n - 1;
+            count++;
+            if (count > 1) return false;
+        }
+
+        return true;
+        */
+        return n > 0 && (n & (n - 1)) == 0;
+    }
 }
