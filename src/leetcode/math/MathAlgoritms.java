ac705eb9de7739551d5e8b26fb6fa99b415317a9
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-04-25 22:30:11 -0500

    Added new solutions

--- src/leetcode/math/MathAlgoritms.java
+++ src/leetcode/math/MathAlgoritms.java
@@ -751,4 +751,13 @@ public class MathAlgoritms {
         return greatest;
     }
 
+    public int minOperations(int[] nums, int k) {
+
+        int sum = 0;
+
+        for (int num : nums) sum += num;
+
+        return sum % k;
+    }
+
 }
