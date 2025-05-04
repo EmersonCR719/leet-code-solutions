10e6b07d3370b70d12c1e2313f938aea98a5a1f6
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-05-03 23:12:09 -0500

    Added new solutions

--- src/leetcode/greedy/Greedy.java
+++ src/leetcode/greedy/Greedy.java
@@ -1,6 +1,8 @@
 package leetcode.greedy;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Greedy {
 
@@ -43,15 +45,6 @@ public class Greedy {
         return 0;
     }
 
-    public int reverse(int x) {
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-
-
-        return 0;
-    }
-
     public int minIncrementForUnique(int[] nums) {
 
         int count = 0;
@@ -70,6 +63,27 @@ public class Greedy {
         return count;
     }
 
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int count = 0;
 
+        Map<Integer, Integer> freqTops = new HashMap<>();
+        Map<Integer, Integer> freqBottoms = new HashMap<>();
 
+        for (int i = 0; i < tops.length; i++) {
+            freqTops.put(tops[i], freqTops.getOrDefault(tops[i], 0) + 1);
+            freqBottoms.put(bottoms[i], freqBottoms.getOrDefault(bottoms[i], 0) + 1);
+        }
+
+        int maxFreqTopNum = -(int) 10e9;
+        int maxFreqBottomNum = -(int) 10e9;
+
+        for (int i = 1; i <= 6; i++) {
+            maxFreqTopNum = Math.max(maxFreqTopNum, freqTops.getOrDefault(i, 0));
+            maxFreqBottomNum = Math.max(maxFreqBottomNum, freqBottoms.getOrDefault(i, 0));
+        }
+
+        System.out.println(maxFreqTopNum + " " + maxFreqBottomNum);
+
+        return count;
+    }
 }
