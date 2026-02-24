208a16fbb29370b575aae46ff4962e48354f0df9
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2026-02-23 20:47:46 -0500

    Added new solutions

--- src/leetcode/array/Arrays.java
+++ src/leetcode/array/Arrays.java
@@ -3654,4 +3654,17 @@ public class Arrays {
 
         return maxFreqNum;
     }
+
+    int differenceK(int []arr, int k){
+        int count = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (Math.abs(arr[i] - arr[j]) == k)
+                    count++;
+            }
+        }
+
+        return count;
+    }
 }
--- src/leetcode/array/Main.java
+++ src/leetcode/array/Main.java
@@ -11,13 +11,15 @@ public class Main {
 
         int[] nums1 = {1000000000,999999999,1000000000,999999999,1000000000,999999999};
 
-        int[] nums2 = {1, 5, 2, 6};
+        int[] nums2 = {1, 7, 5, 9, 2, 12, 3};
 
         String s1 = "abcd", s2 = "dcba";
 
         List<String> list = new ArrayList<>(List.of("one.two.three"));
 
-        System.out.println(arr.splitWordsBySeparator(list, '.'));
+        //System.out.println(arr.splitWordsBySeparator(list, '.'));
+
+        System.out.println(arr.differenceK(nums2,2));
 
     }
 }
